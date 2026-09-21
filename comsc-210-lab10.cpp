6c7bac721006e46e340e3369c338b94fd6458cf1
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-20 18:24:06 -0700

    Second commit

---
+++ comsc-210-lab10.cpp
@@ -0,0 +1,16 @@
+// COMSC-210 | Lab 11 | Ahmad Dharhan
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Cars {
+    string brand;
+    string country;
+    int 
+
+};
+
+int main(){
+
+}

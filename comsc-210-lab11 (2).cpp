2525a8e25974aab05e9643473e121e97dda52443
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-20 20:16:52 -0700

    Thirteenth commit

--- comsc-210-lab11 (2).cpp
+++ comsc-210-lab11 (2).cpp
@@ -38,6 +38,13 @@ int main(){
     for(int i = 0; i < NUM_CARS; i++){
         displayinfo(&total[i], visit[i], i + 1);
     }
+
+    for(int i = 0; i < NUM_CARS; i++){
+        delete[] total[i].mileage;
+    }
+
+    delete[] total;
+
     return 0;
 
 }
@@ -46,12 +53,19 @@ int inputinfo(Cars *total, int val){
     int visits;
 
     cout << "Enter customer #" << val << " info:" << endl;
+
     cout << "Brand: ";
     getline(cin, total -> brand);
+    cin.ignore();
+
     cout << "Country Car Is Manufactured: ";
     getline(cin, total ->country);
+    cin.ignore();
+
     cout << "Model: ";
     getline(cin, total -> model);
+    cin.ignore();
+
     cout << "Year: ";
     cin >> total -> year;
 
@@ -61,7 +75,7 @@ int inputinfo(Cars *total, int val){
     total -> mileage = new int[visits];
 
     for(int i = 0; i < visits; i++){
-        cout << "Enter mileage for visit #" << i << endl;
+        cout << "Enter mileage for visit #" << i + 1 << endl;
         cin >> total -> mileage[i];
     }
 
@@ -70,11 +84,17 @@ int inputinfo(Cars *total, int val){
 }
 
 void displayinfo(Cars *total, int numvisits, int customnum){
-    cout << "Number #" << customnum << endl;;
-    cout << "Brand: " << total -> brand;
-    cout << "Model: " << total -> model;
-    cout << "Year: " << total -> year;
+    cout << "Number #" << customnum << endl;
+    cout << "Brand: " << total -> brand << endl;
+    cout << "Model: " << total -> model << endl;
+    cout << "Year: " << total -> year << endl;
 
+    cout << "Mileage: ";
 
+    for(int i = 0; i < numvisits; i++){
+        cout << total -> mileage[i] << " ";
+    }
+
+    cout << endl;
 
 }
